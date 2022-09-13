6f33d1ef53927086934615767fa02f5ab5350fb5
DOMESDAY <79838340+DOMESDAY7@users.noreply.github.com>
2022-09-13 19:09:07 +0200

    add command

--- api.js
+++ api.js
@@ -1,36 +1,44 @@
 import fetch from "node-fetch";
-import ical2json from "ical2json"
-import { format, compareDesc, parseISO } from 'date-fns'
+import ical2json from "ical2json";
+import { format, compareDesc, parseISO } from "date-fns";
+
+// fichier contenant toutes les interactions avec le fichier
 
 let url =
   "https://planif.esiee.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=3172&projectId=10&calType=ical&nbWeeks=52";
 
-let cours_data;
+export async function getCoursesAt(date) {
+  return new Promise((resolve, reject) => {
+    let cours_data;
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/text",
+      },
+    })
+      .then((resp) => {
+        return resp.text();
+      })
+      .then((resp) => {
+        let jsonCal = ical2json.convert(resp);
 
-fetch(url, {
-  headers: {
-    "Content-Type": "application/text",
-  },
-})
-  .then((resp) => {
-    return resp.text()
-  }) 
-  .then((resp)=>{
-    let jsonCal = ical2json.convert(resp);
-    // la variable cours_data contient les cours mis sous forme json
-    cours_data = jsonCal.VCALENDAR[0].VEVENT;
-    console.table(getCoursesAt("09/13/2022"))
-  })
+        // la variable cours_data contient les cours mis sous forme json
+        cours_data = jsonCal.VCALENDAR[0].VEVENT;
 
-  function getCoursesAt(date) {
-    let res = [];
-    let target = format(new Date(date), 'dd-mm-yyyy')
+        let res = [];
+        let target = format(new Date(date), "dd-mm-yyyy");
 
-    cours_data.forEach(cours => {
-      let tmp = format(new Date(parseISO(cours.DTSTART)), 'dd-mm-yyyy')
-      if(tmp === target) {
-        res.push(cours)
-      }
-    });
-    return res;
-  }
+        cours_data.forEach((cours) => {
+          let tmp = format(new Date(parseISO(cours.DTSTART)), "dd-mm-yyyy");
+          if (tmp === target) {
+            res.push(cours);
+          }
+        });
+
+        // on reverse le tableau pour avoir les matières dans l'ordre
+        res.reverse();
+        resolve(res);
+      })
+      .catch((e) => reject(e));
+  });
+}
+getCoursesAt("09/13/2022").then((resp) => console.log(resp));
--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 import DiscordJS from "discord.js";
 import dotenv from "dotenv";
-
+import getCoursesAt from "./api";
 dotenv.config();
 
 const client = new DiscordJS.Client({
-    intents: [
-		DiscordJS.GatewayIntentBits.Guilds,
-		DiscordJS.GatewayIntentBits.GuildMessages,
-		DiscordJS.GatewayIntentBits.MessageContent,
-		DiscordJS.GatewayIntentBits.GuildMembers,
-	],
-})
-
-console.log("Starting...")
-
-// on bot ready
-client.on("ready",()=>{
-    console.log("🤖 The bot is online")
-})
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-    }
-    if (commandName === 'aujd') {
-		await interaction.reply("Tes cours d'aujourd'hui sont :\nBLABLABLA");
-    }
+  intents: [
+    DiscordJS.GatewayIntentBits.Guilds,
+    DiscordJS.GatewayIntentBits.GuildMessages,
+    DiscordJS.GatewayIntentBits.MessageContent,
+    DiscordJS.GatewayIntentBits.GuildMembers,
+  ],
 });
 
-client.login(process.env.BOT_TOKEN)
+console.log("Starting...");
+
+// on bot ready
+client.on("ready", () => {
+  console.log("🤖 The bot is online");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+  if (commandName === "aujd") {
+    await interaction.reply("Tes cours d'aujourd'hui sont :\nBLABLABLA");
+  }
+  if (commandName === "!ajd") {
+    await interaction.reply(
+      "tes cours sont : " +
+        getCoursesAt(new Date()).then((cours) =>
+          cours.map((cours) => cours.SUMMARY + "en" + cours.LOCATION + "\n")
+        )
+    );
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
